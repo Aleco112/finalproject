b1d4847a7a296820b09aee65e0949e0b42b6eacf
odettelittle <Odettelittle1@gmail.com>
2022-05-10 02:17:19 +0000

    primercommit

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../../styles/home.css";
+import Recipes  from "./Recipes.jsx";
+
+export const Home = () => {
+  return <div className="text-center mt-5"><Recipes/></div>;
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,9 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Recipes Temple</h1>
 		<p>
-			<img src={rigoImage} />
+			<img src="https://as1.ftcdn.net/v2/jpg/01/65/38/54/1000_F_165385492_8aqTKbAXMX7daHuthK74kywA5S3KmZtl.jpg" />
 		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
 	</div>
 );

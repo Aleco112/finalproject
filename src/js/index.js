a2b2eedaf5c6c041dc49281979fec9e4086793a6
danyez1 <cdanielyanez144@gmail.com>
2022-05-12 21:01:28 +0000

    api

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,26 @@ import "../styles/index.css";
 
 //import your own components
 import Layout from "./layout";
+import  {initializeApp}  from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const fireBaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.projectId,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
+
+// Initialize Firebase
+export const myApp = initializeApp(fireBaseConfig);
+
+//render your react application
+ReactDOM.render(<Layout />, document.querySelector("#app"));
 
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));

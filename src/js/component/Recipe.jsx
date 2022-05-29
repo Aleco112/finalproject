1d992fe944040bc25c3bd913c06efd9f6365ece8
odettelittle <Odettelittle1@gmail.com>
2022-05-29 23:32:53 +0000

    ingredients and instructions recipe component

--- src/js/component/Recipe.jsx
+++ src/js/component/Recipe.jsx
@@ -16,35 +16,32 @@ export const Recipe = () => {
       {store.recipe.meals &&
         store.recipe.meals.map((ndata, index) => {
           return (
-            <div key={index} className="img-fluid rounded-circle ms-2" style={{ width: "18rem" }}>
+            <div key={index} className="img-fluid rounded-circle ms-2" style={{ width: "30rem" }}>
               <img src={ndata.strMealThumb} className="img-fluid rounded-circle ms-2" />
               <h5 className="card-title">{ndata.strMeal}</h5>
               <ul>
-                <li>{ndata.strIngredient1} {ndata.strMeasure1}</li>
-                <li>{ndata.strIngredient2} {ndata.strMeasure2}</li>
-                <li>{ndata.strIngredient3} {ndata.strMeasure3}</li>
-                <li>{ndata.strIngredient4} {ndata.strMeasure4}</li>
-                <li>{ndata.strIngredient5} {ndata.strMeasure5}</li>
-                <li>{ndata.strIngredient6} {ndata.strMeasure6}</li>
-                <li>{ndata.strIngredient7} {ndata.strMeasure7}</li>
-                <li>{ndata.strIngredient8} {ndata.strMeasure8}</li>
-                <li>{ndata.strIngredient9} {ndata.strMeasure9}</li>
-                <li>{ndata.strIngredient10} {ndata.strMeasure10}</li>
-                <li>{ndata.strIngredient11} {ndata.strMeasure11}</li>
-                <li>{ndata.strIngredient12} {ndata.strMeasure12}</li>
-                <li>{ndata.strIngredient13} {ndata.strMeasure13}</li>
-                <li>{ndata.strIngredient14} {ndata.strMeasure14}</li>
-                <li>{ndata.strIngredient15} {ndata.strMeasure15}</li>
-                <li>{ndata.strIngredient16} {ndata.strMeasure16}</li>
-                <li>{ndata.strIngredient17} {ndata.strMeasure17}</li>
-                <li>{ndata.strIngredient18} {ndata.strMeasure18}</li>
-                <li>{ndata.strIngredient20} {ndata.strMeasure20}</li>
-                <li>{ndata.strIngredient21} {ndata.strMeasure21}</li>
-                <li>{ndata.strIngredient22} {ndata.strMeasure22}</li>
-
+                {ndata.strIngredient1} {ndata.strMeasure1}
+                {ndata.strIngredient2} {ndata.strMeasure2}
+                {ndata.strIngredient3} {ndata.strMeasure3}
+                {ndata.strIngredient4} {ndata.strMeasure4}
+                {ndata.strIngredient5} {ndata.strMeasure5}
+                {ndata.strIngredient6} {ndata.strMeasure6}
+                {ndata.strIngredient7} {ndata.strMeasure7}
+                {ndata.strIngredient8} {ndata.strMeasure8}
+                {ndata.strIngredient9} {ndata.strMeasure9}
+                {ndata.strIngredient10} {ndata.strMeasure10}
+                {ndata.strIngredient11} {ndata.strMeasure11}
+                {ndata.strIngredient12} {ndata.strMeasure12}
+                {ndata.strIngredient13} {ndata.strMeasure13}
+                {ndata.strIngredient14} {ndata.strMeasure14}
+                {ndata.strIngredient15} {ndata.strMeasure15}
+                {ndata.strIngredient16} {ndata.strMeasure16}
+                {ndata.strIngredient17} {ndata.strMeasure17}
+                {ndata.strIngredient18} {ndata.strMeasure18}
+                {ndata.strIngredient20} {ndata.strMeasure20}
               </ul>
-              <p>{ndata.strIngredient1}</p>
-              {ndata.strIngredient16 !== "" && <p>Ingrediente: {ndata.strIngredient16}</p>}
+             <p>{ndata.strInstructions}</p>
+             <span>Tags:{ndata.strTags}</span>    
             </div>
 
 

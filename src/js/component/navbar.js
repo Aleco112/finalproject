f8b1b3b7da5965eb1a61360c66f995f56db1e5fd
Aleco112 <aleco112@yahoo.fr>
2022-05-29 22:21:36 +0000

    contact

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,14 +5,14 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <Link to="/" className="navbar-brand">Login</Link>
+    <Link to="/login" className="navbar-brand">Login</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link to="/login" className="nav-link active" aria-current="page" href="#">Home</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Contact us!</a>
